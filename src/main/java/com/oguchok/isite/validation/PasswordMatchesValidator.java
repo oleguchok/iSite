12fb88c0fad7a116563a1b1bc59d3fe321545013
oleguhcok <oleguchok@gmail.com>
2015-07-31 11:30:08 +0300

    Add password matches validator

---
+++ src/main/java/com/oguchok/isite/validation/PasswordMatchesValidator.java
@@ -0,0 +1,22 @@
+package com.oguchok.isite.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.oguchok.isite.service.UserDTO;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+	@Override
+	public void initialize(PasswordMatches arg0) {
+		
+	}
+
+	@Override
+	public boolean isValid(Object obj, ConstraintValidatorContext context) {
+		
+		UserDTO user = (UserDTO) obj;
+		return user.getPassword().equals(user.getMathcingPassword());
+	}
+	
+}
